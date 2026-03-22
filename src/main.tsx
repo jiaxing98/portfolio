50e0b530703afdfac55f5ad9dcdbe99e1c1fe0d4
Jia Xing Wan <jxwan98@gmail.com>
2026-03-22 14:28:41 +0800

    Added tanstack router and gsap

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollSmoother } from 'gsap/ScrollSmoother'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { SplitText } from 'gsap/SplitText'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Home } from './home/Home.tsx'
 import './index.css'
+
+// Import the generated route tree
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+// register the useGSAP hook to avoid React version discrepancies
+gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother, SplitText)
+
+// Create a new router instance
+const router = createRouter({ routeTree })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </StrictMode>
 )
